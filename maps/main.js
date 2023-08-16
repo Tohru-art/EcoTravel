19c1a312f10ae995b8ffdcd6e7eb0dbe07ead684
MatthewASC23 <matthewd.delacruz07@gmail.com>
2023-08-16 15:11:06 -0400

    changes

--- maps/main.js
+++ maps/main.js
@@ -1,6 +1,12 @@
 let genBtn = document.getElementById('gen_btn');
+let message = document.getElementById('result');
 
-genBtn.onclick = function(){
-    let img = document.createElement('img');
+genBtn.onclick = function(event){
+    event.preventDefault()
+    // let imgTag = document.createElement('img');
+    // genBtn.src = "transport-img/bike.jpeg";
+    // imgTag.append(genBtn.src);
+
+    message.innerHTML = 'The fastest way to get there would be by bike';
     
 }
