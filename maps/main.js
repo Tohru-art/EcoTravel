311fb5fd9040fe41a00e33576dc2199b7455242e
MatthewASC23 <matthewd.delacruz07@gmail.com>
2023-08-16 13:16:44 -0400

    changes

--- maps/main.js
+++ maps/main.js
@@ -1,7 +1,6 @@
-// function myMap() {
-//     var mapProp= {
-//       center:new google.maps.LatLng(51.508742,-0.120850),
-//       zoom:5,
-//     };
-//     var map = new google.maps.Map(document.getElementById("map"),mapProp);
-// }
+let genBtn = document.getElementById('gen_btn');
+
+genBtn.onclick = function(){
+    let img = document.createElement('img');
+    
+}
